edd27981e28f18c038fabd5ddf74c87cdf50ed26
pranan050 <nandagopalpradhyun@gmail.com>
2024-10-19 13:47:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from src.download import download_and_unzip_kaggle_dataset
 from src.process import process
+
 def main():
 
   # Download the Data
   download_and_unzip_kaggle_dataset('jancsg/cybersecurity-suspicious-web-threat-interactions')
-
+  
   # Import and Organize the Data
-
+  
   # Visualize the Data
   
 
